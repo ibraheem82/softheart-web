9b2f1a52145491dbe8878c2628bd1cd68b2db243
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-07-08 18:33:04 +0100

    app configured properly...

--- softheart/softheart/settings.py
+++ softheart/softheart/settings.py
@@ -39,8 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'softapp'
-]
+    'softapp.apps.SoftappConfig'
+ ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
