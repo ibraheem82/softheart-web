ec034591447dc38bfb5ae2da638ba430eed8fc7e
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-07-12 16:38:55 +0100

    changes made..

--- softheart/softapp/models.py
+++ softheart/softapp/models.py
@@ -9,7 +9,6 @@ class Course(models.Model):
    title              = models.CharField(max_length=200)
    description        = models.TextField(null=True, blank=True)
    course_thumbnail   = models.ImageField(validators = [FileExtensionValidator(['png', 'jpg'])], null=True, blank=True)
-   initial_price      = models.IntegerField(default=0)
    current_price      = models.IntegerField(default=0)
    category           = models.ManyToManyField('Category')
    id                 = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
