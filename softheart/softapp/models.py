8e8efd8fd4d4ac82d91b7c93961aebad6bc2f6e9
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-07-16 12:28:09 +0100

    where images should be uploaded was configured...

--- softheart/softapp/models.py
+++ softheart/softapp/models.py
@@ -4,11 +4,18 @@ from django.core.validators import FileExtensionValidator
 # Create your models here.
 
 
-
+def upload_path(instance, filename):
+    # Define the upload path for the course thumbnail
+    # You can customize this according to your requirements
+    return f'course_thumbnails/{instance.id}/{filename}'
+ 
+ 
+ 
+ 
 class Course(models.Model):
    title              = models.CharField(max_length=200)
    description        = models.TextField(null=True, blank=True)
-   course_thumbnail   = models.ImageField(validators = [FileExtensionValidator(['png', 'jpg'])], null=True, blank=True)
+   course_thumbnail   = models.ImageField(upload_to=upload_path, validators = [FileExtensionValidator(['png', 'jpg'])], null=True, blank=True)
    current_price      = models.IntegerField(default=0)
    category           = models.ManyToManyField('Category')
    id                 = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
