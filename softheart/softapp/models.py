19695237afe4d4796cdfd32335641dc10ca62f0f
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-07-16 12:31:35 +0100

    image resizing logic was done...

--- softheart/softapp/models.py
+++ softheart/softapp/models.py
@@ -21,6 +21,35 @@ class Course(models.Model):
    current_price      = models.IntegerField(default=0)
    category           = models.ManyToManyField('Category')
    id                 = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+   def save(self, *args, **kwargs):
+         super().save(*args, **kwargs)
+         if self.course_thumbnail:
+            # Resize the uploaded image after saving the model instance
+            self.resize_image()
+
+   def resize_image(self):
+        # Open the uploaded image using PIL
+        image = Image.open(self.course_thumbnail.path)
+
+        # Define the desired dimensions for the resized image
+        # You can customize these dimensions according to your requirements
+        max_width = 800
+        max_height = 600
+
+        # Resize the image while preserving the aspect ratio
+        image.thumbnail((max_width, max_height), Image.ANTIALIAS)
+
+        # Save the resized image, overwriting the original file
+        image.save(self.course_thumbnail.path)
+
+        # You can also save the resized image to a different location if needed
+        # new_image_path = f'some/other/path/{self.course_thumbnail.name}'
+        # image.save(new_image_path)
+
+        # Update the image field to reflect the resized image
+        self.course_thumbnail.name = os.path.basename(self.course_thumbnail.path)
+        self.save()
+
 
    def __str__(self):
         return self.title
